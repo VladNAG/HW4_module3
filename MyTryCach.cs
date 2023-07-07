4a0429140fb44112f90a74d2b68bb5b28b7b585f
Vladyslav Nahornyi <vladnagornyj2@gmail.com>
2023-07-07 11:04:28 +0100

    Solved

---
+++ MyTryCach.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4_module3
+{
+    public class MyTryCach
+    {
+        public MyTryCach()
+        {
+            Event = GetSum;
+            Event += GetSum;
+        }
+
+        public delegate void EventHandler(int x, int y);
+        public delegate void DisplaySum();
+        public event EventHandler Event;
+        public int Sum { get; private set; }
+        public void SumEvent(int x, int y)
+        {
+            Event(x, y);
+            Console.WriteLine(Sum);
+        }
+
+        public void Try(DisplaySum x)
+        {
+            try
+            {
+                x();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ex EX EX");
+            }
+        }
+
+        private void GetSum(int x, int y)
+        {
+            Sum += x + y;
+        }
+    }
+}
---
+++ Starter1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4_module3
+{
+    public static class Starter1
+    {
+        public static void Start()
+        {
+            var test = new MyTryCach();
+            test.SumEvent(5, 4);
+        }
+    }
+}
